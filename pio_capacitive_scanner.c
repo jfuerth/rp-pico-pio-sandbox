a3c43a54a996fb5742c56aa38b88f3f23e5e3c81
Jonathan Fuerth <jfuerth@gmail.com>
2026-03-07 21:46:46 -0500

    WIP: preparing to use DMA to update the PIO registers for each pin
    At this stage, I'm using the CPU and the standard SDK functions to change
    the pin number (but not modifying the program) after each scan. The next
    step in this plan is to trigger a DMA when we push the value to the rx queue,
    which will copy the value out and chain a second DMA that updates the PIO
    registers to point to the next pin, restarting the PIO program in the process
    by writing to the exec register or program counter register.

--- pio_capacitive_scanner.c
+++ pio_capacitive_scanner.c
@@ -9,59 +9,33 @@
 #define NUM_KEYS 4
 #define FIRST_KEY_PIN 0
 
-// State machines for each key
-static uint sm_for_key[NUM_KEYS];
-static PIO pio_for_key[NUM_KEYS];
+static inline void capacitive_touch_program_set_pin(PIO pio, uint sm, uint offset, uint pin) {
+    // Get default state machine config
+    pio_sm_config c = capacitive_touch_program_get_default_config(offset); // TODO pass in a config to modify (we don't need to reset all these things every time if we're just changing the pin)
+    
+    // Configure the pin for SET operations
+    sm_config_set_set_pins(&c, pin, 1);
+    
+    // Configure the pin for JMP PIN test
+    sm_config_set_jmp_pin(&c, pin);
+    
+    // Set clock divider to ~1 MHz (125 MHz / 125 = 1 MHz)
+    // This makes each PIO cycle = 1 microsecond
+    sm_config_set_clkdiv(&c, 125.0f);
+    
+    // Configure autopush: we're using manual push, so disable autopush
+    sm_config_set_in_shift(&c, false, false, 32);
+    sm_config_set_out_shift(&c, false, false, 32);
 
-// Initialize a capacitive touch sensor on a given pin
-void init_capacitive_sensor(uint key_index, uint pin) {
-    // For simplicity, we'll use PIO0 for all sensors
-    // Each sensor gets its own state machine
-    PIO pio = pio0;
-    
-    // Try to claim a state machine
-    int sm = pio_claim_unused_sm(pio, false);
-    if (sm < 0) {
-        // If PIO0 is full, try PIO1
-        pio = pio1;
-        sm = pio_claim_unused_sm(pio, true); // Will panic if none available
-    }
-    
-    // Load the PIO program (only load once per PIO)
-    static uint offset_pio0 = 0;
-    static uint offset_pio1 = 0;
-    static bool loaded_pio0 = false;
-    static bool loaded_pio1 = false;
-    
-    uint offset;
-    if (pio == pio0) {
-        if (!loaded_pio0) {
-            offset = pio_add_program(pio, &capacitive_touch_program);
-            offset_pio0 = offset;
-            loaded_pio0 = true;
-        } else {
-            offset = offset_pio0;
-        }
-    } else {
-        if (!loaded_pio1) {
-            offset = pio_add_program(pio, &capacitive_touch_program);
-            offset_pio1 = offset;
-            loaded_pio1 = true;
-        } else {
-            offset = offset_pio1;
-        }
-    }
-    
-    // Initialize the state machine
-    capacitive_touch_program_init(pio, sm, offset, pin);
-    
-    // Store the PIO and SM for later use
-    pio_for_key[key_index] = pio;
-    sm_for_key[key_index] = sm;
-    
-    printf("Initialized key %d on GPIO %d using PIO%d SM%d\n", 
-           key_index, pin, pio_get_index(pio), sm);
+    // Set the pin to input mode initially (high-Z) and give control to PIO
+    gpio_set_pulls(pin, false, false);  // Disable internal pulls (relying on external pull-up)
+    pio_gpio_init(pio, pin); // TODO we should only need to do this once per pin, not every time we change the pin for the program
+    pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, false);
+
+    // apply the config and set the initial PC to the start of our program (halts the PIO SM)
+    pio_sm_init(pio, sm, offset, &c);
 }
+
 
 int main() {
     // Initialize stdio for printf
@@ -74,11 +48,22 @@ int main() {
     
     // Small delay to let serial connection stabilize
     sleep_ms(1000);
+
+    // Just grabbing PIO0 for now
+    // Each sensor gets its own state machine
+    PIO pio = pio0;
+    uint pio_program_offset = pio_add_program(pio, &capacitive_touch_program);
     
+    // Try to claim a state machine
+    int sm = pio_claim_unused_sm(pio, true); // Will panic if not available
+
+    printf("Initialized PIO%d SM%d with program at offset %d\n", pio_get_index(pio), sm, pio_program_offset);
+
+    // TODO perform one-time setup of pins here (set for usage with PIO, disable pull up/down, etc) so we don't have to do it every time we change the pin for the program
     // Initialize all capacitive sensors
-    for (uint i = 0; i < NUM_KEYS; i++) {
-        init_capacitive_sensor(i, FIRST_KEY_PIN + i);
-    }
+    // for (uint i = 0; i < NUM_KEYS; i++) {
+    //     init_capacitive_sensor(i, FIRST_KEY_PIN + i);
+    // }
     
     printf("\nStarting continuous scanning...\n\n");
     
@@ -89,19 +74,24 @@ int main() {
     while (true) {
         // Read from all sensor FIFOs
         for (uint i = 0; i < NUM_KEYS; i++) {
-            PIO pio = pio_for_key[i];
-            uint sm = sm_for_key[i];
+            printf("\nReading key %d on pin %d...\n", i, FIRST_KEY_PIN + i);
+            capacitive_touch_program_set_pin(pio, sm, pio_program_offset, i + FIRST_KEY_PIN);
             
+            // Start the state machine
+            pio_sm_set_enabled(pio, sm, true);
+            printf("Waiting for data...");
+
             // Wait for data to be available in FIFO
             while (pio_sm_is_rx_fifo_empty(pio, sm)) {
                 tight_loop_contents();
             }
             
+            printf("Data available! Reading... ");
             // Read the cycle count from FIFO
             // PIO counts DOWN from 0xFFFFFFFF, so actual count = MAX - reading
             uint32_t countdown_value = pio_sm_get_blocking(pio, sm);
-            // readings[i] = 0xFFFFFFFF - countdown_value;
-            readings[i] = countdown_value;
+            readings[i] = 0xFFFFFFFF - countdown_value;
+            printf("Done! Count: %lu\n", readings[i]);
         }
         
         // Print all readings on one line
